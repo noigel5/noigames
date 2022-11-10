c4d46db60d394dc8c63f2429f4d5f32235b1bb3b
b044544 <flavio.fiechter@baloise.ch>
2022-11-10 14:26:21 +0100

    hard drop, rotate

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -11,30 +11,51 @@ tetrisboard_ctx.canvas.height = ROWS * BLOCK_SIZE;
 tetrisboard_ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 const KEY = {
-    LEFT: 37,
-    RIGHT: 39,
-    DOWN: 40
+    LEFT: 'ArrowLeft',
+    RIGHT: 'ArrowRight',
+    DOWN: 'ArrowDown',
+    SPACE: 'Space',
+    UP: 'ArrowUp'
 }
 Object.freeze(KEY);
 
 const moves = {
-    [KEY.LEFT]: p => ({...p, x: p.x - 1}), //TODO: ?????
+    [KEY.LEFT]: p => ({...p, x: p.x - 1}),
     [KEY.RIGHT]: p => ({...p, x: p.x + 1}),
-    [KEY.DOWN]: p => ({...p, y: p.y + 1})
+    [KEY.DOWN]: p => ({...p, y: p.y + 1}),
+    [KEY.SPACE]: p => ({...p, y: p.y + 1}),
+    [KEY.UP]: p => ({...p})
 };
 
 document.addEventListener('keydown', event => {
-    if (moves[event.keyCode]) {
+    if (moves[event.code]) {
         event.preventDefault();
 
-        let p = moves[event.keyCode](board.piece);
+        let p = moves[event.code](board.piece);
+
+        if (event.code === KEY.UP) {
+            board.piece.rotate();
+        }
 
         if (board.valid(p)) {
-            board.piece.move(p);
+            if (event.code === KEY.SPACE) {
+                while (board.valid(p)) {
+                    board.piece.move(p);
+                    p = moves[KEY.DOWN](board.piece);
+                }
+            } else {
+                board.piece.move(p);
+            }
 
             tetrisboard_ctx.clearRect(0, 0, tetrisboard_ctx.canvas.width, tetrisboard_ctx.canvas.height);
 
             board.piece.draw();
+        } else {
+            if (event.code === KEY.UP) {
+                board.piece.rotate();
+                board.piece.rotate();
+                board.piece.rotate();
+            }
         }
     }
 });
@@ -47,12 +68,10 @@ class Board {
 
     insideWalls(x) {
         return !(x > 9 || x < 0);
-
     }
 
     aboveFloor(y) {
         return y <= 19;
-
     }
 
     reset() {
@@ -66,7 +85,6 @@ class Board {
     }
 
     valid(p) {
-        debugger
         return p.shape.every((row, dy) => {
             return row.every((value, dx) => {
                 let x = p.x + dx;
@@ -110,6 +128,18 @@ class Piece {
     move(p) {
         this.x = p.x;
         this.y = p.y;
+    }
+
+    rotate() {
+        for (let y = 0; y < this.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+                [this.shape[x][y], this.shape[y][x]] = [this.shape[y][x], this.shape[x][y]];
+            }
+        }
+
+        this.shape.forEach(row => row.reverse());
+
+        return this;
     }
 }
 
